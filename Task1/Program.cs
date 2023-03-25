74967c61e0b9ce7b7815039fb48b2bd53623843e
SPECTR <ivanov8465@mail.ru>
2023-03-25 23:34:55 +0300

    Сделал 1 задание

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()!;
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i + 1}-й элемент ");
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.WriteLine($"a[{i}] = {array[i]}");
+}
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.Clear();
+int length = Prompt("Введите количество элементов: ");
+int[] array;
+array = InputArray(length);
+PrintArray(array);
+Console.WriteLine($"Количество положительных чисел: {CountPositiveNumbers(array)}");
+
